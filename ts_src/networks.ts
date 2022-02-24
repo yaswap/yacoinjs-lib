6912ea731d599227c9e21093a8976526716ee3ee
Chau Nguyen <chaunguyen4392@gmail.com>
2022-02-24 23:59:55 +0700

    [network] Adapt with yacoin networks

--- ts_src/networks.ts
+++ ts_src/networks.ts
@@ -1,5 +1,5 @@
-// https://en.bitcoin.it/wiki/List_of_address_prefixes
-// Dogecoin BIP32 is a proposed standard: https://bitcointalk.org/index.php?topic=409731
+// https://en.yacoin.it/wiki/List_of_address_prefixes
+// Dogecoin BIP32 is a proposed standard: https://yacointalk.org/index.php?topic=409731
 export interface Network {
   messagePrefix: string;
   bech32: string;
@@ -14,19 +14,19 @@ interface Bip32 {
   private: number;
 }
 
-export const bitcoin: Network = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
+export const yacoin: Network = {
+  messagePrefix: '\x18Yacoin Signed Message:\n',
   bech32: 'bc',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x00,
-  scriptHash: 0x05,
+  pubKeyHash: 0x4D,
+  scriptHash: 0x8B,
   wif: 0x80,
 };
 export const regtest: Network = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  messagePrefix: '\x18Yacoin Signed Message:\n',
   bech32: 'bcrt',
   bip32: {
     public: 0x043587cf,
@@ -37,7 +37,7 @@ export const regtest: Network = {
   wif: 0xef,
 };
 export const testnet: Network = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  messagePrefix: '\x18Yacoin Signed Message:\n',
   bech32: 'tb',
   bip32: {
     public: 0x043587cf,

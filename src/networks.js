88122c93bae0e672819ab16fa55a9fd745e07c81
Chau Nguyen <chaunguyen4392@gmail.com>
2022-05-22 12:36:29 +0700

    fixup! [network] Make yacoin testnet same as mainnet

--- src/networks.js
+++ src/networks.js
@@ -25,12 +25,12 @@ exports.regtest = {
 };
 exports.testnet = {
   messagePrefix: '\x18Yacoin Signed Message:\n',
-  bech32: 'tb',
+  bech32: 'bc',
   bip32: {
-    public: 0x043587cf,
-    private: 0x04358394,
+    public: 0x0488b21e,
+    private: 0x0488ade4,
   },
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
-  wif: 0xef,
+  pubKeyHash: 0x4d,
+  scriptHash: 0x8b,
+  wif: 0xcd,
 };
